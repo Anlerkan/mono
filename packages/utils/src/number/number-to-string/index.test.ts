0b55ebf6354c6ffee3cb524d55f480997ead7a08
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-11-16 14:56:06 +0300

    build: Initialize project

---
+++ packages/utils/src/number/number-to-string/index.test.ts
@@ -0,0 +1,30 @@
+import numberToString from ".";
+
+describe("Valid numbers should return it's string value", () => {
+  test('numberToString: 1 => "1"', () => {
+    expect(numberToString(1)).toBe("1");
+  });
+
+  test('numberToString: 0 => "0"', () => {
+    expect(numberToString(0)).toBe("0");
+  });
+
+  test('numberToString: 1.23 => "1.23"', () => {
+    // eslint-disable-next-line no-magic-numbers
+    expect(numberToString(1.23)).toBe("1.23");
+  });
+});
+
+describe("Non-valid values should return empty string", () => {
+  test('numberToString: null => ""', () => {
+    expect(numberToString(null)).toBe("");
+  });
+
+  test('numberToString: undefined => ""', () => {
+    expect(numberToString(undefined)).toBe("");
+  });
+
+  test('numberToString: NaN => ""', () => {
+    expect(numberToString(NaN)).toBe("");
+  });
+});
