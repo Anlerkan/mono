52b39654da1f9c39b400bdf1661017327dda92ae
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-11-16 20:38:03 +0300

    build: commitlint improvements

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
+const {
+  utils: {getPackages}
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+} = require("@commitlint/config-lerna-scopes");
+
 module.exports = {
-  extends: ["@commitlint/config-conventional", "@commitlint/config-lerna-scopes"]
+  extends: ["@commitlint/config-conventional", "@commitlint/config-lerna-scopes"],
+  rules: {
+    "scope-enum": async (ctx) => [
+      // eslint-disable-next-line no-magic-numbers
+      2,
+      "always",
+      [
+        ...(await getPackages(ctx)),
+        // Insert custom scopes below:
+        "release"
+      ]
+    ]
+  }
 };
